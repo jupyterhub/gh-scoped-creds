61d6aabe72e2595a2171f4ec5ecbe11129384a03
YuviPanda <yuvipanda@gmail.com>
2022-04-17 00:22:22 -0700

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gh-scoped-creds",
-    version="2.1",
+    version="2.2",
     url="https://github.com/yuvipanda/gh-scoped-creds",
     license="3-clause BSD",
     author="Yuvi Panda",
